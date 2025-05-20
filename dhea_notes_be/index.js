1ea1e1a9fed778e4b2f0b506cd08b0a2212c9bfb
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-05-21 01:14:55 +0700

    Update index.js

--- dhea_notes_be/index.js
+++ dhea_notes_be/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import CatatanRoute from "./routes/CatatanRoute.js"
+import CatatanRoute from "./routes/CatatanRoute.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import sequelize from "./config/database.js"; // ✅ PAKAI INI
+
+dotenv.config();
 
 const app = express();
 app.set("view engine", "ejs");
 
-dotenv.config();
-
 app.use(cookieParser());
-app.use(cors({credentials:true, origin:'https://e-13-450704.uc.r.appspot.com'}));
+app.use(cors({ credentials: true, origin: 'https://e-13-450704.uc.r.appspot.com' }));
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 app.use(UserRoute);
@@ -19,13 +20,12 @@ app.use(CatatanRoute);
 
 const start = async () => {
   try {
-    await sequelize.authenticate();
+    await sequelize.authenticate(); // ✅ INI YANG BENAR
     console.log("Database connected");
-    await sequelize.sync(); // sinkronisasi model
+    await sequelize.sync();
 
-    // Menggunakan PORT dari environment atau default ke 5000
     const port = process.env.PORT || 5000;
-    app.listen(port, '0.0.0.0',() => console.log(`Server running on port ${port}`));
+    app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
