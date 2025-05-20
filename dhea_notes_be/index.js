fa40c3387c4a8568fc47b785e03b522e5ce4ee55
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-05-20 22:22:01 +0700

    Update index.js

--- dhea_notes_be/index.js
+++ dhea_notes_be/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import CatatanRoute from "./routes/CatatanRoute.js"
+import CatatanRoute from "./routes/CatatanRoute.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -10,12 +10,24 @@ app.set("view engine", "ejs");
 
 dotenv.config();
 
+// Aktifkan cookie parser
 app.use(cookieParser());
-app.use(cors({credentials:true, origin:'https://e-13-450704.uc.r.appspot.com'}));
+
+const corsOptions = {
+  origin: "https://e-13-450704.uc.r.appspot.com",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+app.options("*", cors(corsOptions));
 app.use(express.json());
+
+// Route
 app.get("/", (req, res) => res.render("index"));
 app.use(UserRoute);
 app.use(CatatanRoute);
 
-const PORT = process.env.PORT || 5000;
+// ✅ Gunakan PORT dari environment (Cloud Run default ke 8080)
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
