dadec43e8443259535f8bc926b5b90964a3bdeaa
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-05-21 04:43:36 +0700

    Update utils.js

--- dhea_notes_fe/src/utils.js
+++ dhea_notes_fe/src/utils.js
@@ -1,7 +1,7 @@
 // src/utils.js
 import axios from 'axios';
 
-export const BASE_URL = 'https://dhea-notes-be-103949415038.us-central1.run.app';
+export const BASE_URL = 'https://notes-be-dhea-103949415038.us-central1.run.app';
 
 export const API = axios.create({
   baseURL: BASE_URL,
